f020c818f2f4f4bb28fba0be2c95c373619836c6
cidicy <126729004+cidicy@users.noreply.github.com>
2024-06-07 22:48:36 +1000

    push

--- app/page.js
+++ app/page.js
@@ -36,16 +36,6 @@ export default function Home() {
        <br/> ANSWER 2: What I wanted to learn or do more of at work is to get know my colleagues better. I wanted to build relationship
        to as many as I could because I know that it will help me with my work.
        </p>
-       <p>
-        <br/> ANSWER 3: I was running a change which was handed over from the different region. The case had been a very long pending case.
-      Because I was new, I wasn't familiar with some hicups tha may happen.
-        So, as I was running the script from the UI, the page just froze up and went on a blank screen. I wasn't sure if the script
-        ran successfully and I couldn't re-run as it will mess up the system. Then I thought of checking the localhost logs. From there,
-        I was able to see that the script was still running and I jus need to monitor it from the logs instead. 
-        </p>
-        <p>
-        <br/> 5 least favourite support task:
-        </p>
       </div>
 
       <div className={styles.center}>
