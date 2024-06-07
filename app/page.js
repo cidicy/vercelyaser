e864e2aaa73b396a28f125633b7a144efcb2c16b
cidicy <126729004+cidicy@users.noreply.github.com>
2024-06-07 21:23:00 +1000

    push

--- app/page.js
+++ app/page.js
@@ -28,6 +28,11 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
+      <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
       <Image
         src="/vercel.svg"
         alt="Vercel Logo"
